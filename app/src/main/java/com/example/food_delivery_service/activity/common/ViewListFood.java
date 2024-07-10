46c66e361aebca4704ebbfb449ff2a880c9eb926
thanhdqhe163134 <thanhdqhe163134@fpt.edu.vn>
2024-07-10 23:33:54 +0700

    merge trungAnh into thanhdq2

--- app/src/main/java/com/example/food_delivery_service/activity/common/ViewListFood.java
+++ app/src/main/java/com/example/food_delivery_service/activity/common/ViewListFood.java
@@ -1,17 +1,12 @@
 package com.example.food_delivery_service.activity.common;
 
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.food_delivery_service.R;
-import com.example.food_delivery_service.adapter.ProductAdapter;
+import com.example.food_delivery_service.adapter.ListFoodAdapter;
 import com.example.food_delivery_service.api.ApiClient;
 import com.example.food_delivery_service.api.ApiService;
 import com.example.food_delivery_service.api.model.dto.ApiResponse;
@@ -26,22 +21,18 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ViewListFood extends AppCompatActivity {
-    private ProductAdapter productAdapter;
+    private ListFoodAdapter productAdapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_view_list_food);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        ListView listView = findViewById(R.id.listfood);
-        productAdapter = new ProductAdapter(this, new ArrayList<>());
-        listView.setAdapter(productAdapter);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        productAdapter = new ListFoodAdapter(new ArrayList<>());
+        recyclerView.setAdapter(productAdapter);
 
         getAllProducts();
     }
@@ -55,15 +46,13 @@ public class ViewListFood extends AppCompatActivity {
             public void onResponse(Call<ApiResponse<ProductsResponse>> call, Response<ApiResponse<ProductsResponse>> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     List<Product> products = response.body().getData().getProducts();
-                    productAdapter.clear();
-                    productAdapter.addAll(products);
+                    productAdapter.updateData(products);
                 }
             }
 
             @Override
             public void onFailure(Call<ApiResponse<ProductsResponse>> call, Throwable t) {
-                Toast.makeText(ViewListFood.this, "Failed to get products", Toast.LENGTH_SHORT).show();
-                t.printStackTrace();
+                // Handle failure
             }
         });
     }
