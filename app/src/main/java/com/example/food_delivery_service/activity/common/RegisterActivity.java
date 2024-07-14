2dea81e525befa34b7f4e693c80671555e19f5f5
thanhdqhe163134 <thanhdqhe163134@fpt.edu.vn>
2024-07-14 21:25:56 +0700

    MERGE MASTER INTO THANHDQ2

---
+++ app/src/main/java/com/example/food_delivery_service/activity/common/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.food_delivery_service.activity.common;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.food_delivery_service.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_user_register_screen1);
+    }
+
+}
