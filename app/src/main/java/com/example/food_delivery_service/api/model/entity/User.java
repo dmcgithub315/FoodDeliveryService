538a1cf789d984023d758decfdb073feb5e0e2a7
kinhtn_adtrue <kinhtn@adtrue.com>
2024-07-15 11:41:21 +0700

    feat: register + update profile

--- app/src/main/java/com/example/food_delivery_service/api/model/entity/User.java
+++ app/src/main/java/com/example/food_delivery_service/api/model/entity/User.java
@@ -1,4 +1,3 @@
-// User.java
 package com.example.food_delivery_service.api.model.entity;
 
 import androidx.annotation.Nullable;
@@ -58,5 +57,131 @@ public class User {
     @Nullable
     private String phone;
 
+    @SerializedName("password")
+    @Nullable
+    private String password;
 
+    // Getter và Setter
+
+    @Nullable
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@Nullable String password) {
+        this.password = password;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    @Nullable
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Nullable String email) {
+        this.email = email;
+    }
+
+    @Nullable
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@Nullable String username) {
+        this.username = username;
+    }
+
+    @Nullable
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(@Nullable String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Nullable
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(@Nullable String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Nullable
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(@Nullable String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Nullable
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@Nullable String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Nullable
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@Nullable String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Nullable
+    public String getEmailVerified() {
+        return emailVerified;
+    }
+
+    public void setEmailVerified(@Nullable String emailVerified) {
+        this.emailVerified = emailVerified;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    @Nullable
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(@Nullable String salt) {
+        this.salt = salt;
+    }
+
+    @Nullable
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(@Nullable String phone) {
+        this.phone = phone;
+    }
 }
